d6a5538f23d87282c4bb2dd9c57b1f3601908dd1
Shane <143796207+Shane0821@users.noreply.github.com>
2026-02-10 20:22:08 +0800

    refactor: use logger

--- demo/demo1/main.cpp
+++ demo/demo1/main.cpp
@@ -22,11 +22,11 @@ int main(int argc, char* argv[]) {
     TcpServer server(&evloop);
 
     server.start(port, [](TcpConn* conn) {
-        std::cout << "New connection established\n";
+        SHLOG_INFO("new connection restablished");
 
         conn->setReadCallback([conn]() {
             auto msg = conn->readAll();
-            std::cout << "Received: " << msg.data_ << std::endl;
+            SHLOG_INFO("received: {}", msg.data_);
             conn->send("HTTP/1.1 200 OK\nContent-Length: 12\n\nHello World!\n", 50);
         });
         // conn->SetReadCallback([conn]() {
